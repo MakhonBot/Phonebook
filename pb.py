a11596e0d857fba0b10c31aaa1ae6132238918e3
MakhonBot <tahiasaiyara@gmail.com>
2025-12-12 21:29:03 +0600

    yo paboz

--- pb.py
+++ pb.py
@@ -57,3 +57,44 @@ def add_contact(pb):
 def remove_existing(pb):
     query = str(input("Please enter the name of the contact you wish to remove..."))
     temp = 0
+    for i in range(len(pb)):
+        if query == pb[i][0]:
+            temp += 1
+            print(pb.pop(i))
+            print("This query has now been removed.")
+            return pb
+    if temp == 0:
+        print("Sorry, you've entered an invalid query. Please try again later.")
+        return pb
+def delete_all(pb):
+    return pb.clear
+def search_existing(pb):
+    choice = int(input("Enter search criteria... \n \n \n1. Name \n2. Number \n3. E-mail \n4.DOB \n5. Category (family/work/friends/other) \nPlease enter..."))
+    temp = []
+    check = -1
+    if choice == 1:
+        query = str(input("Please enter name..."))
+        for i in range(len(pb)):
+            if query == pb[i][0]:
+                check = i
+                temp.append(pb[i])
+    
+    elif choice == 2:
+        query = int(input("Please enter number..."))
+        for i in range(len(pb)):
+            if query == pb[i][1]:
+                check = i
+                temp.append(pb[i])
+    elif choice == 3:
+        query = str(input("Please enter email..."))
+        for i in range(len(pb)):
+            if query == pb[i][2]:
+                check = i
+                temp.append(pb[i])
+    elif choice == 4:
+        query = str(input("Please enter DOB (dd/mm/yyyy)..."))
+        for i in range(len(pb)):
+            if query == pb[i][3]:
+                check = i
+                temp.append(pb[i])
+    
